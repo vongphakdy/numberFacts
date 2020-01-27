52e964001facc3106730b662df21840db3c52882
Isaac Vongphakdy <IVongphakdy@gmail.com>
2020-01-26 22:13:24 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ numberInput.addEventListener('input', getFactFetch);
 function getFactFetch(){
     let number = numberInput.value;
 
-    fetch('http://numbersapi.com/' + number)
+    fetch('https://numbersapi.com/' + number)
         .then(response => response.text())
         .then(data => {
             if(number != ''){
